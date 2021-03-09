6cd358efd80066c52695b16fc3e1d064f8400bc1
LxkdGithub <l3023269814@163.com>
2021-03-09 16:40:17 +0800

    'commit'

--- Scripts/net.py
+++ Scripts/net.py
@@ -216,7 +216,7 @@ def main():
     model = Net()
     model = nn.DataParallel(model)
     model = model.to(device)
-    if args.load-model and os.path.exists("VideoFake_cnn.pt"):
+    if args.load_model and os.path.exists("VideoFake_cnn.pt"):
         model.load_state_dict(torch.load("VideoFake_cnn.pt"))
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     scheduler = StepLR(optimizer, step_size=5000, gamma=args.gamma)    # TODO 衰减速度
